ca1c54263e929dcc89113b3dba05be430cac1536
Reiji <reiji.210@icloud.com>
2022-06-24 18:31:29 +0900

    change function name

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 # import motors
-import IDA_star
+# import IDA_star
 # import Two_hase_Algorithm
 import random
 
-def Set_random_scramble(random_count):
-    move_names = "U U' U2 D D' D2 L L' L2 R R' R2 F F' F2 B B' B2"
+def Create_scramble(scramble_length):
+    move_names = "U U' U2 D D' D2 L L' L2 R R' R2 F F' F2 B B' B2 P"
     random_scramble = []
-    for i in range(random_count):
+    for i in range(scramble_length):
         random_scramble += random.choice(move_names)
     print('random_scramble = ', random_scramble)
     return random_scramble
 
 if __name__ == '__main__':
-    random_count = 20
-    random_scramble = Set_random_scramble(random_count)
+    scramble_length = 20
+    scramble = Create_scramble(scramble_length)
 
     # motor = motors.Motor()
+    # motor.Solve(scramble)
